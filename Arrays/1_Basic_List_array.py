4835361f9ed06b420b926b05dd67b1cc1fc44fcf
soham0005 <sohamadhyapak0005@gmail.com>
2023-06-01 15:47:56 +0530

    File 1 Updated with basic programs

--- Arrays/1_Basic_List_array.py
+++ Arrays/1_Basic_List_array.py
@@ -1,3 +1,32 @@
-arr = [1,2,3,4,5,6]
-arr.clear()
+arr = [1,19,3,4,5,36]
+# arr.clear()
 print(arr)
+
+
+# Swapping the elements of the array
+[arr[0],arr[1]] = [arr[1],arr[0]]
+print(arr)
+
+
+# Max item
+print(max(arr))
+
+# max item in range 0:4
+print(max(arr[:4]))
+
+# Reversing the array
+print(arr[::-1])
+
+# Reversing the array using two pointers approach
+def reverseArray(arr):
+    start = 0
+    end = len(arr) - 1
+    
+    while start < end:
+        [arr[start],arr[end]] = [arr[end],arr[start]]
+        start +=1 
+        end -= 1
+        
+    print("Reversed Array is :",arr)
+    
+reverseArray([1,2,3,4,5,6,67,7,8]) 
