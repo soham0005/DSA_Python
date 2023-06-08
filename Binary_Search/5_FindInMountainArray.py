dab8df4cdaef6d86d1644d61213da37700570a01
soham0005 <sohamadhyapak0005@gmail.com>
2023-06-08 16:06:58 +0530

    Binary Search Updated

---
+++ Binary_Search/5_FindInMountainArray.py
@@ -0,0 +1,8 @@
+# Find Element in Mountain array - Leetcode 1095
+
+''''
+Approach :- Find Peak Element
+Apply Binary Search in First Half form 0 to peak Index
+If element not found then apply binary search in second Half which is in decreasing order.
+
+'''
