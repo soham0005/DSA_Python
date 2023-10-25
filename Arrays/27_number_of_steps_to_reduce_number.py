2d7d165949374daaf133277ffa502482641cd8eb
soham0005 <sohamadhyapak0005@gmail.com>
2023-10-25 18:31:31 +0530

    Files Added

---
+++ Arrays/27_number_of_steps_to_reduce_number.py
@@ -0,0 +1,12 @@
+
+def numberOfSteps(num):
+    count = 0
+    while num!=0:
+        if(num % 2 == 0):
+            num = num // 2
+            count+=1
+        else:
+            num = num - 1
+            count += 1
+    print(count)
+numberOfSteps(8)
